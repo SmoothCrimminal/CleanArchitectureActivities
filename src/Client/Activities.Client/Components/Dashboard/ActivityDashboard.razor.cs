63f0b06c7e3a945e8b1f9c33d3435df849e9fb3a
Marcin2 <marcin.krupinski@icsolutions.pl>
2024-01-19 19:11:10 +0100

    added navbar

---
+++ src/Client/Activities.Client/Components/Dashboard/ActivityDashboard.razor.cs
@@ -0,0 +1,11 @@
+﻿using Activities.Models.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace Activities.Client.Components.Dashboard
+{
+    public partial class ActivityDashboard
+    {
+        [Parameter, EditorRequired]
+        public IEnumerable<ActivityDto>? Activities { get; set; }
+    }
+}
